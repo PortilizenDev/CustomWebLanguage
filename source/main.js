4aae3e1b8936cb3860b3957f65c52f9a8ff4cef1
Madness-Rombank <113909588+PortilizenDev@users.noreply.github.com>
2024-08-15 12:27:12 -0500

    just trying to get the text displaying

--- source/main.js
+++ source/main.js
@@ -1,14 +1,7 @@
-const begin = Date.now(); // First Line
-
 import {split as splitCode, run as runProgram} from './language/runner.js'
 
 splitCode()
 runProgram()
-
-const end = Date.now(); // Last PROGRAM RUNNING, Line.
-
-const timetaken = end
-timetaken = timetaken - begin
 
 const timeReport = document.createElement('p')
 const node = document.createTextNode("This is new.");
